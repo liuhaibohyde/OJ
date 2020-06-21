a4a9308e05d8c04f1034146d9529598ba3076d35
Hyde <liuhaibohyde@hotmail.com>
2020-06-21 17:39:04 +0800

    add some comments

--- src/Algorithm/strStr.cpp
+++ src/Algorithm/strStr.cpp
@@ -1,5 +1,5 @@
-#include <vector>
-#include <string>
+#include <vector>	
+#include <string>	
 
 using namespace std;
 
@@ -9,29 +9,28 @@ public:
         if (needle.empty()) {
             return 0;
         }
-
         if (haystack.empty()) {
             return -1;
         }
 
         // 构造KMP数组
-        int n = needle.size();
-        vector<vector<int>> dp(n + 1, vector<int>(128));
-        dp[0][needle[0]] = 1;
+        int len = needle.size();
+        vector<vector<int>> dp(len + 1, vector<int>(256, 0));   // dp[当前状态(字符下标index)][下一个字符] = 下一个状态(字符下标index)
+        dp[0][needle[0]] = 1;                                   // 初始状态，只有匹配到needle中的第一个字符才会“状态前进”
         int x = 0;
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < 128; ++j)  {
-                dp[i][j] = dp[x][j];
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < 256; j++) {
+                dp[i][j] = dp[x][j];    // 遍历ASCII表赋初值为上一个状态，即X的状态；后续实际匹配中，如果当前字符匹配不到，则回去找X的状态
             }
-            dp[i][needle[i]] = i + 1;
-            x = dp[x][needle[i]];
+            dp[i][needle[i]] = i + 1;   // 匹配到needle中的字符，状态前进
+            x = dp[x][needle[i]];       // x表示的是与当前i具有相同前缀的上一个状态
         }
-
+        
         x = 0;
-        for (int i = 0; i < haystack.size(); ++i) {
+        for (int i = 0; i < haystack.size(); i++) {
             x = dp[x][haystack[i]];
-            if (x == n) {
-                return i - n + 1;
+            if (x == len) {
+                return i - len + 1;
             }
         }
         return -1;
